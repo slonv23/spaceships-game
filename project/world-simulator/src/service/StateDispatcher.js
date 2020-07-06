2ee1d70bb04a3eebf12794d55c3839d9d8841114
Illia Solonar <illia.solonar@gmail.com>
2020-07-06 23:36:11 +0300

    Fixes

--- project/world-simulator/src/service/StateDispatcher.js
+++ project/world-simulator/src/service/StateDispatcher.js
@@ -1,6 +1,6 @@
 /**
  * @typedef {import('./SocketServer')} SocketServer
- * @typedef {import('../engine/state/StateManager').default} StateManager
+ * @typedef {import('../engine/state/AuthoritativeStateManager').default} AuthoritativeStateManager
  * @typedef {import('../engine/net/format/MessageSerializerDeserializer').default} MessageSerializerDeserializer
  * @typedef {import('../engine/physics/object/FlyingObject').default} FlyingObject
  * @typedef {import('../engine/object-control/flying-object/RemoteFlyingObjectController').default} RemoteFlyingObjectController
@@ -15,12 +15,12 @@ const logger = require('../utils/logger');
 class StateDispatcher {
 
     /**
-     * @param {StateManager} stateManager
+     * @param {AuthoritativeStateManager} authoritativeStateManager
      * @param {SocketServer} socketServer
      * @param {MessageSerializerDeserializer} messageSerializerDeserializer
      */
-    constructor(stateManager, socketServer, messageSerializerDeserializer) {
-        this.stateManager = stateManager;
+    constructor(authoritativeStateManager, socketServer, messageSerializerDeserializer) {
+        this.stateManager = authoritativeStateManager;
         this.socketServer = socketServer;
         this.messageSerializerDeserializer = messageSerializerDeserializer;
         this.lastDispatchTimestamp = Date.now();
@@ -28,14 +28,14 @@ class StateDispatcher {
 
     handleStateUpdated = () => {
         const timestamp = Date.now();
-        if ((timestamp - this.lastDispatchTimestamp) > packetPeriodMs) {
+        if ((timestamp - this.lastDispatchTimestamp) >= packetPeriodMs) {
             logger.debug('Dispatching new state');
             this.lastDispatchTimestamp = timestamp;
-            this.dispatchState();
+            this.dispatchState(this.stateManager.currentFrameIndex);
         }
     };
 
-    dispatchState() {
+    dispatchState(frameIndex) {
         const objectStates = [];
         for (/** @type {RemoteFlyingObjectController} */ const objectController of this.stateManager.controllers) {
             /** @type {FlyingObject} */
@@ -51,12 +51,14 @@ class StateDispatcher {
             objectState.angularVelocity = object.angularVelocity;
             objectState.position = object.position;
             objectState.quaternion = object.quaternion;
+            objectState.actions = [];
 
             objectStates.push(objectState);
         }
 
         const worldState = new WorldState();
         worldState.objectStates = objectStates;
+        worldState.frameIndex = frameIndex;
 
         const serializedResponse = this.messageSerializerDeserializer.serializeResponse(worldState, {requestId: 0});
         this.socketServer.broadcast(serializedResponse);
